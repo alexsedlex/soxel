5c6331f96451f37471e27c763d2e361435048635
Alex Morel <amorel@codelutin.com>
2021-06-17 21:34:34 +0200

    file selection'

--- src/services/Constants.ts
+++ src/services/Constants.ts
@@ -19,23 +19,4 @@
  * #L%
  */
 
-export default class Constants {
-
-    static baseApiUrl():string {
-        let result = process.env.VUE_APP_API_URL;
-        if (!result) {
-            result = location.protocol + "//" + location.hostname;
-            if (process.env.VUE_APP_API_DEFAULT_PORT) {
-                result += ":" + process.env.VUE_APP_API_DEFAULT_PORT
-            }
-            result += "/api";
-        }
-        return result;
-    }
-
-    static apiUrl(path:string):string {
-        const result = Constants.baseApiUrl() + path;
-        return result;
-    }
-
-}
+export default class Constants {}
--- src/views/SoxelMapperVue.vue
+++ src/views/SoxelMapperVue.vue
@@ -19,16 +19,123 @@
   #L%
   -->
 <template>
-  <div class="lakes"></div>
+  <b-steps
+    v-model="activeStep"
+    :animated="true"
+    :rounded="true"
+    :clickable="true"
+    :has-navigation="false"
+  >
+    <b-step-item step="1" label="Fichier Base" :clickable="true">
+      <div class="content">
+        <h1 class="title has-text-centered">Fichier Base</h1>
+
+        <b-upload v-model="baseFile" class="file-label" accept=".ods" multiple>
+          <span class="file-cta">
+            <b-icon class="file-icon" icon="upload"></b-icon>
+            <span class="file-label"> Renseignez le fichier Excel Base</span>
+          </span>
+          <span class="file-name" v-if="baseFile.length > 0">
+            {{ baseFile[0].name }}
+          </span>
+        </b-upload>
+      </div>
+    </b-step-item>
+
+    <b-step-item
+      step="2"
+      label="Réponse Bloctel"
+      :clickable="baseFile.length > 0"
+    >
+      <div class="content">
+        <h1 class="title has-text-centered">Réponse Bloctel</h1>
+
+        <b-upload
+          v-model="answerFile"
+          class="file-label"
+          accept=".ods"
+          multiple
+        >
+          <span class="file-cta">
+            <b-icon class="file-icon" icon="upload"></b-icon>
+            <span class="file-label"> Renseignez le fichier Bloctel</span>
+          </span>
+          <span class="file-name" v-if="answerFile.length > 0">
+            {{ answerFile[0].name }}
+          </span>
+        </b-upload>
+      </div>
+    </b-step-item>
+
+    <b-step-item
+      step="3"
+      label="Résultat"
+      :clickable="baseFile.length > 0 && answerFile.length > 0"
+    >
+      <div class="content">
+        <h1 class="title has-text-centered">Résultat</h1>
+
+        <b-loading v-model="isCalculating" />
+        <span v-if="isCalculating">
+          Calcul en cours, ce service vous êtes offert par votre dévoué boudin
+          créole...</span
+        >
+        <div v-else>
+          <b-message type="is-success">Calcul terminé !</b-message>
+          <b-button @click="download">Télécharger le résultat </b-button>
+        </div>
+      </div>
+    </b-step-item>
+  </b-steps>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 
 @Component({})
-export default class SoxelMapperVue extends Vue {}
+export default class SoxelMapperVue extends Vue {
+  activeStep = 0;
+  baseFile: File[] = [];
+  answerFile: File[] = [];
+  resultFile: File[] = [];
+  isCalculating = false;
+
+  @Watch("baseFile")
+  baseFileChanged(): void {
+    if (this.baseFile.length > 0) {
+      this.activeStep = 1;
+    }
+  }
+
+  @Watch("answerFile")
+  anwserFileChanged(): void {
+    if (this.answerFile.length > 0) {
+      this.launchCalculation();
+      this.activeStep = 2;
+    }
+  }
+
+  launchCalculation(): void {
+    this.isCalculating = true;
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const that = this;
+    setTimeout(() => {
+      that.resultFile = that.baseFile;
+      that.isCalculating = false;
+    }, 5000);
+  }
+
+  download(): void {
+    console.error("DONE LOAD");
+  }
+}
 </script>
 
 <style scoped lang="less">
 @import "../less/main";
+
+.content {
+  margin: auto;
+  width: 500px;
+}
 </style>

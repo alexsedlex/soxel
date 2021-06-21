b51add6a1d318cf888f79964574f8ba7bd2c0434
Alex Morel <amorel@codelutin.com>
2021-06-21 09:44:59 +0200

    github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // options...
-  outputDir: 'target/dist-' + process.env.DIST_FOLDER,
+  publicPath: 'soxel',
   // Adding this allows to debug in firefox/chrome
   configureWebpack: {
     devtool: 'source-map'

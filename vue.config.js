9b50bc1813c56f0cef69f1c75221d3873bcbc3ab
Alex Morel <amorel@codelutin.com>
2021-06-21 09:47:40 +0200

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // options...
-  publicPath: 'soxel',
+  publicPath: '/soxel/',
   // Adding this allows to debug in firefox/chrome
   configureWebpack: {
     devtool: 'source-map'
